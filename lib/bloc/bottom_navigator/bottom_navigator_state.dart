05c9e8211828ed00e6e818ba771b560463776ea0
mahla1385 <m.abiri1385@gmail.com>
2025-05-04 17:53:57 +0330

    Delete

--- lib/bloc/bottom_navigator/bottom_navigator_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'bottom_navigator_cubit.dart';
-
-@immutable
-sealed class BottomNavigatorState {}
-
-final class BottomNavigatorInitial extends BottomNavigatorState {
-  final int page;
-  BottomNavigatorInitial({required this.page});
-}
--- lib/utils/theme_style.dart
+++
@@ -1,9 +0,0 @@
-import 'package:music/gen/fonts.gen.dart';
-import 'package:flutter/material.dart';
-
-mixin ThemeStyle {
-  static TextStyle get normalStyle => TextStyle(
-      fontFamily: FontFamily.diodrumArabic, fontSize: 14, letterSpacing: 0, wordSpacing: 0);
-
-}
-
